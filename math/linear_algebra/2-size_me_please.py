90bc1b6b801f65707a32167b8cb17ddfd0ec9fbd
narmingasimli <nerminqasimli210@gmail.com>
2025-08-11 16:33:20 +0000

    add and update 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 def matrix_shape(matrix):
     shape = []
     current_dimension = matrix
-
     while isinstance(current_dimension, list):
         shape.append(len(current_dimension))
-
         if len(current_dimension) > 0:
             current_dimension = current_dimension[0]
         else:
