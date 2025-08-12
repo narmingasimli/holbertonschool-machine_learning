6917c66c4cb52d88514626626b7ddda4bae028d1
narmingasimli <nerminqasimli210@gmail.com>
2025-08-12 12:24:41 +0000

    add

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,14 +2,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def line():
     y = np.arange(0, 11) ** 3
-    
     plt.figure()
-    
     plt.plot(y, 'r-')
-    
     plt.xlim([0, 10])
-    
-    plt.savefig('0-line.png');
+    plt.savefig('0-line.png')
