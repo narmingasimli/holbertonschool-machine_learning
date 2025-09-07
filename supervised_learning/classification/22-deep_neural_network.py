a9746ab913d47418bbba4d9986c67f02b65e302b
narmingasimli <nerminqasimli210@gmail.com>
2025-09-07 17:50:56 +0000

    update 22-deep_neural_network.py

--- supervised_learning/classification/22-deep_neural_network.py
+++ supervised_learning/classification/22-deep_neural_network.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Classification algorithm using Deep Neural Network (DNN class)."""
+
+
 import numpy as np
 
 
@@ -7,7 +9,7 @@ class DeepNeuralNetwork:
     """Deep Neural Network class."""
 
     def __init__(self, nx, layers):
-        """Init Function"""
+        """Construct the deep neural network object."""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -19,7 +21,7 @@ class DeepNeuralNetwork:
         self.__cache = {}
         self.__weights = {}
 
-        for i in range(self.__L):
+        for i in range(self.L):
             if not isinstance(layers[i], int) or layers[i] <= 0:
                 raise TypeError("layers must be a list of positive integers")
 
@@ -33,53 +35,50 @@ class DeepNeuralNetwork:
 
     @property
     def L(self):
-        """Getter for number of layers."""
+        """Getter Method"""
         return self.__L
+
 
     @property
     def cache(self):
-        """Getter for cache dictionary."""
+        """Getter Method"""
         return self.__cache
+
 
     @property
     def weights(self):
-        """Getter for weights dictionary."""
+        """Getter Method"""
         return self.__weights
 
+
     def forward_prop(self, X):
-        """Calculates forward propagation for the deep neural network"""
-        self.__cache["A0"] = X
+        """Forward propagation"""
+        self.__cache['A0'] = X
+        for i in range(1, self.__L + 1):
+            W = self.__weights['W{}'.format(i)]
+            A = self.__cache['A{}'.format(i - 1)]
+            b = self.__weights['b{}'.format(i)]
+            z = np.dot(W, A) + b
+            self.__cache['A{}'.format(i)] = 1 / (1 + np.exp(-z))
 
-        for layer in range(1, self.__L + 1):
-            W = self.__weights[f"W{layer}"]
-            b = self.__weights[f"b{layer}"]
-            A_prev = self.__cache[f"A{layer-1}"]
+        return self.__cache['A{}'.format(self.__L)], self.__cache
 
-            Z = np.dot(W, A_prev) + b
-            A = 1 / (1 + np.exp(-Z))  # sigmoid activation
-
-            self.__cache[f"A{layer}"] = A
-
-        return A, self.__cache
 
     def cost(self, Y, A):
-        """Calculates the cost of the model using logistic regression"""
+        """Cost Function"""
         m = Y.shape[1]
-        cost = -(1/m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
-        return cost
+        J = -(1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return J
+
 
     def evaluate(self, X, Y):
-        """Evaluates the neural network's predictions"""
-        # Perform forward propagation
-        A, _ = self.forward_prop(X)
+        """Evaluate"""
+        self.forward_prop(X)
+        e = self.__cache['A{}'.format(self.__L)]
+        cost = self.cost(Y, e)
+        labels = np.where(e >= 0.5, 1, 0)
+        return labels, cost
 
-        # Compute cost
-        cost = self.cost(Y, A)
-
-        # Convert activated outputs to binary predictions
-        predictions = np.where(A >= 0.5, 1, 0)
-
-        return predictions, cost
 
     def gradient_descent(self, Y, cache, alpha=0.05):
         """Gradient descent Function"""
@@ -98,19 +97,19 @@ class DeepNeuralNetwork:
             self.__weights['W{}'.format(i)] -= alpha * dwl
             self.__weights['b{}'.format(i)] -= alpha * dbl
 
-    def train(self, X, Y, iterations=5000, alpha=0.05):
-        """train"""
-        if not isinstance(iterations, int):
-            raise TypeError("iterations must be an integer")
-        if iterations < 1:
-            raise ValueError("iterations must be a positive integer")
 
-        if not isinstance(alpha, float):
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """Train Function"""
+        if type(iterations) is not int:
+            raise TypeError("iterations must be an integer")
+        if iterations <= 0:
+            raise ValueError("iterations must be a positive integer")
+        if type(alpha) is not float:
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
-
-        for i in range(iterations):
+        for _ in range(iterations):
+            cache_l, cache = self.forward_prop(X)
+            self.gradient_descent(Y, cache, alpha)
             self.forward_prop(X)
-            self.gradient_descent(Y, self.__cache, alpha)
         return self.evaluate(X, Y)
