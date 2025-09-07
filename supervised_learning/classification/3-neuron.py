fb735b9da2256aa28f22a82a84de81e2fd1dd598
narmingasimli <nerminqasimli210@gmail.com>
2025-09-07 15:55:33 +0000

    add 3-neuron.py

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -13,32 +13,26 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
         self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
-
     @property
     def W(self):
         """Getter Method"""
         return self.__W
-
     @property
     def b(self):
         """Getter Method"""
         return self.__b
-
     @property
     def A(self):
         """Getter Method"""
         return self.__A
-
     def forward_prop(self, X):
         """Forward Propagation"""
         Z = np.dot(self.__W, X) + self.__b
         self.__A = 1 / (1 + np.exp(-Z))
         return self.__A
-
     def cost(self, Y, A):
         """Cost Function"""
         m = Y.shape[1]
