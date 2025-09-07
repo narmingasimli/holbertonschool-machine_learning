d27362833eb2b07420cce45979f99539232b3611
narmingasimli <nerminqasimli210@gmail.com>
2025-09-07 15:39:02 +0000

    add 0-neuron.py

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Classification"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """Neuron Class"""
+
+    def __init__(self, nx):
+        """Init Function"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
