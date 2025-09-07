12670c77bd9c800ca8ddf0a2ba7a9fba91ffe415
narmingasimli <nerminqasimli210@gmail.com>
2025-09-07 16:03:41 +0000

    add 5-neuron.py

---
+++ supervised_learning/classification/5-neuron.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+
+"""Classification"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """Neuron Class"""
+
+    def __init__(self, nx):
+        """Init Function"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter Method"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter Method"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter Method"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Forward Propagation"""
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Cost Function"""
+        m = Y.shape[1]
+        J = -1 / m * np.sum((Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)))
+        return J
+
+    def evaluate(self, X, Y):
+        """Evaluate Prediction"""
+        A = self.forward_prop(X)
+        pred = np.where(A >= 0.5, 1, 0)
+        cost = self.cost(Y, A)
+        return pred, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """Gradient Descent"""
+        m = Y.shape[1]
+        dZ = A - Y
+        dW = 1 / m * np.dot(dZ, X.T)
+        db = (1/m) * np.sum(dZ)
+        self.__W = self.__W - (alpha * dW)
+        self.__b = self.__b - (alpha * db)
