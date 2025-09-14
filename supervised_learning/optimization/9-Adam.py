f213c8a5a93ab431c5a6eae3b0410a74407f8561
narmingasimli <nerminqasimli210@gmail.com>
2025-09-14 17:12:39 +0000

    add 9-Adam.py

---
+++ supervised_learning/optimization/9-Adam.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Comment of Function"""
+import numpy as np
+
+
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    """Adam Function"""
+    v = beta1 * v + (1 - beta1) * grad
+    s = beta2 * s + (1 - beta2) * (grad ** 2)
+    v_corrected = v / (1 - beta1 ** t)
+    s_corrected = s / (1 - beta2 ** t)
+    var = var - alpha * v_corrected / (np.sqrt(s_corrected) + epsilon)
+    return var, v, s
