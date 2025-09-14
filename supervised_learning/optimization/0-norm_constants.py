e8e0e28e4f4e6a77ce484a66fa1f7a87d4f89e83
narmingasimli <nerminqasimli210@gmail.com>
2025-09-14 16:32:56 +0000

    add 0-norm_constants.py

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -1,24 +1,10 @@
 #!/usr/bin/env python3
-"""
-Optimization
-"""
+"""Normalization Function"""
 import numpy as np
 
 
-class Neuron():
-    """
-    documented
-    """
-
-    def __init__(self, nx):
-        """
-        documented
-        """
-        if type(nx) is not int:
-            raise TypeError("nx must be an integer")
-        if nx < 1:
-            raise ValueError("nx must be a positive integer")
-        self.nx = nx
-        self.W = np.random.randn(1, nx)
-        self.b = 0
-        self.A = 0
+def normalization_constants(X):
+    """Normalization Constants Function"""
+    M = np.mean(X, axis=0)
+    S = np.std(X, axis=0)
+    return M, S
